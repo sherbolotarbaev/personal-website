9ccf278e5e7ee52534a526892da3ce61be600978
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2025-06-12 01:16:41 +0600

    add: animation for blog layout

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,6 @@
+import ContainerWrapper from 'shared/ui/container.wrapper'
 import CopyButton from 'shared/ui/copy-button'
+import SectionWrapper from 'shared/ui/section.wrapper'
 
 interface LayoutBlogPostProps {
 	children: React.ReactNode
@@ -15,7 +17,9 @@ export default function LayoutBlogPost({
 				<CopyButton variant='text' content={() => window.location.href} />
 			</div>
 			<main className='prose prose-gray pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium'>
-				{children}
+				<ContainerWrapper>
+					<SectionWrapper>{children}</SectionWrapper>
+				</ContainerWrapper>
 			</main>
 		</>
 	)
